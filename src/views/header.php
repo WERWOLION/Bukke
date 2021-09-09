b07c85fa6445c30debe822c82c3501be34852299
WERWOLION <88873347+WERWOLION@users.noreply.github.com>
2021-09-09 22:40:42 +0300

    1.2

---
+++ src/views/header.php
@@ -0,0 +1,18 @@
+
+              <div class="heder"><!-- class="heder -->
+                
+                  <a class="heder__logo" href=""></a>
+                  <!-- class="heder-list" -->
+                  <ul class="heder-list"> 
+                  <li class="heder-list__item aktiv"> <a href="" class="heder-list__lnk" >Home</a></li>
+                  <li class="heder-list__item"><a href="" class="heder-list__lnk" >About</a></li>
+                  <li class="heder-list__item"><a href="" class="heder-list__lnk" >Contents</a> </li>
+                  <li class="heder-list__item"><a href="" class="heder-list__lnk" >Books</a></li>
+                  <li class="heder-list__item"><a href="" class="heder-list__lnk" >Education</a></li>
+                  <li class="heder-list__item"><a href="" class="heder-list__lnk" >Skills</a></li>
+                  <li class="heder-list__item"><a href="" class="heder-list__lnk" >Portfolio</a></li>
+                  <li class="heder-list__item"><a href="" class="heder-list__lnk" >Clients</a></li>
+                  <li class="heder-list__item"sx><a href="" class="heder-list__lnk" >Contact</a></li>
+                  </ul> <!--  class="heder-list" -->
+              </div><!-- class="heder -->
+              <div>54554gdfbvzcbbghghhggh</div>
